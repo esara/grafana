eac0c6451b0d76861afcd939af3fd3899856a840
esara <esara@causely.ai>
2025-02-25 22:07:25 -0500

    causely api proxy

--- pkg/plugin/resources.go
+++ pkg/plugin/resources.go
@@ -1,9 +1,115 @@
 package plugin
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
+	"time"
 )
+
+type UserResponse struct {
+	AccessToken  string `json:"accessToken"`
+	RefreshToken string `json:"refreshToken"`
+	ExpiresIn    int    `json:"expiresIn"`
+}
+
+var domain = "causely.app"
+var userName string
+var password string
+
+// authenticate sends a POST request to Frontegg to get the access token.
+func authenticate(userName string, password string) (string, error) {
+	authUrl := fmt.Sprintf("https://auth.%s", domain)
+	loginUrl := fmt.Sprintf("%s/identity/resources/auth/v1/user", authUrl)
+
+	payload := map[string]string{"email": userName, "password": password}
+	payloadBytes, err := json.Marshal(payload)
+	if err != nil {
+		return "", err
+	}
+
+	client := &http.Client{}
+	req, err := http.NewRequest(http.MethodPost, loginUrl, bytes.NewReader(payloadBytes))
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("accept", "application/json")
+	req.Header.Set("Content-Type", "application/json")
+
+	res, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("authentication failed: %s", res.Status)
+	}
+
+	responseData, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", fmt.Errorf("error reading response body: %w", err)
+	}
+
+	var response UserResponse
+	if err = json.Unmarshal(responseData, &response); err != nil {
+		return "", fmt.Errorf("failed to parse response: %w", err)
+	}
+
+	return response.AccessToken, nil
+}
+
+// getGraphQL proxies the GraphQL response to the Causely API and returns the json response.
+func getGraphQL(token string, body io.Reader) ([]byte, error) {
+	baseUrl := fmt.Sprintf("https://api.%s", domain)
+	url := fmt.Sprintf("%s/query", baseUrl)
+	client := &http.Client{Timeout: 10 * time.Second}
+	req, err := http.NewRequest(http.MethodPost, url, body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create request: %w", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("request failed: %w", err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		responseData, _ := io.ReadAll(res.Body) // Optionally log the response.
+		return nil, fmt.Errorf("request failed: %s - %s", res.Status, responseData)
+	}
+
+	return io.ReadAll(res.Body)
+}
+
+// handleQuery is an HTTP POST resource that returns graphql JSON response.
+func (a *App) handleQuery(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	token, err := authenticate(userName, password)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	response, err := getGraphQL(token, req.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Add("Content-Type", "application/json")
+	if _, err := w.Write(response); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
 
 // handlePing is an example HTTP GET resource that returns a {"message": "ok"} JSON response.
 func (a *App) handlePing(w http.ResponseWriter, req *http.Request) {
@@ -39,6 +145,7 @@ func (a *App) handleEcho(w http.ResponseWriter, req *http.Request) {
 
 // registerRoutes takes a *http.ServeMux and registers some HTTP handlers.
 func (a *App) registerRoutes(mux *http.ServeMux) {
+	mux.HandleFunc("/query", a.handleQuery)
 	mux.HandleFunc("/ping", a.handlePing)
 	mux.HandleFunc("/echo", a.handleEcho)
 }
