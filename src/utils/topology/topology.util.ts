d3399c72783e2d5e7af1d85a5deb834d70d88f2f
Viktor Nikitenko <vnikiten01@gmail.com>
2025-06-25 22:23:32 -0400

    feat: [DEV-3453] Adds cluster and namespace name to scope editor

--- src/utils/topology/topology.util.ts
+++ src/utils/topology/topology.util.ts
@@ -8,7 +8,14 @@ export type DeploymentCountLayering =
   | 'assetAndOperation'
   | 'network';
 
+export type TopologyContextInfo = {
+  clusterName?: string;
+};
+
+
 export class TopologyUtil {
+
+  private static AccessingKeyword = ' accessing ';
 
   private static namespaceRestrictedEntityTypesSet: Set<string> = new Set<string>([
     'HTTPPath',
@@ -24,9 +31,56 @@ export class TopologyUtil {
       StringsUtil.containsPath(entity?.name)
     );
   }
-  
+
   public static toHumanizedEntityType(entity: ApiEntity): string {
     return StringsUtil.splitCamelCase(entity?.typeName ?? '');
   }
 
+  public static getNamespaceNames(entity: ApiEntity): string[] {
+    if (!TopologyUtil.hasNamespace(entity)) {
+      return [];
+    }
+
+    // special case when we append 'accessing' in backend
+    if (TopologyUtil.doesEntityNameContainsAccessing(entity?.name)) {
+      return TopologyUtil.splitAccessingEntityName(entity)
+        .filter((part) => StringsUtil.containsPath(part))
+        .map((part) => StringsUtil.getFirstItemOfPath(part))
+        .filter(StringsUtil.isNotBlank);
+    }
+
+    return [StringsUtil.getFirstItemOfPath(entity.name)].filter(StringsUtil.isNotBlank);
+  }
+
+  public static doesEntityNameContainsAccessing(entityName: string): boolean {
+    return StringsUtil.containsIgnoreCase(entityName, TopologyUtil.AccessingKeyword);
+  }
+
+  public static splitAccessingEntityName(entity: ApiEntity): string[] {
+    return entity?.name?.split(TopologyUtil.AccessingKeyword) ?? [];
+  }
+
+  public static getClusterName(entity: ApiEntity): string {
+    const skipKeySet = new Set<string>(['Id', 'k8s.cluster.uid']);
+
+    const knownKeyPropMap = new Map<string, { field: string }>([
+      ['k8s.cluster.name', { field: 'clusterName' }],
+      ['causely.ai/cluster', { field: 'clusterName' }],
+    ]);
+
+    const contextInfo: TopologyContextInfo = (entity?.labels ?? [])
+      .filter((label) => !skipKeySet.has(label.key) && StringsUtil.isNotBlank(label.key) && StringsUtil.isNotBlank(label.value))
+      .reduce(
+        (map, label) => {
+          if (knownKeyPropMap.has(label.key)) {
+            const { field } = knownKeyPropMap.get(label.key);
+            if (StringsUtil.isNotBlank(field)) {
+              map[field] = label.value;
+            }
+          }
+
+          return map;
+        }, {} as TopologyContextInfo);
+        return contextInfo.clusterName ?? '';
+  }
 }
