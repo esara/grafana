3418ffc57382e6046ce7d43285da0739ca0d5f54
Viktor Nikitenko <vnikiten01@gmail.com>
2026-02-19 11:20:35 -0500

    feat: [DEV-4842] Update stylings to use prefix

--- src/panels/components/causely-serviceHealthSummary-panel/serveHealthSummaryContent.component.tsx
+++ src/panels/components/causely-serviceHealthSummary-panel/serveHealthSummaryContent.component.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import { useServiceHealthApi } from "./useServiceHealthApi";
 import { EntityHealthCard, EntityHealthCardData } from "./entityHealthCard.component";
 import { CuiLoadingErrorWrapper } from "sdk/loadingErrorWrapper/cuiLoadingErrorWrapper.component";
+import { SdkUtil } from "sdk/sdk.util";
 
 export const ServiceHealthSummaryContent: React.FC = () => {
     const {isLoading, data, error} = useServiceHealthApi();
 
     return (
         <CuiLoadingErrorWrapper isLoading={isLoading} error={error}>
-            <div className="service-health-summary-panel">
+            <div className={SdkUtil.withPrefix('service-health-summary-panel')}>
                 {data.map((entityHealthCardData: EntityHealthCardData) => {
                     return (
                         <div key={entityHealthCardData.severity}>
